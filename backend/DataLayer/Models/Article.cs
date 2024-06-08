f1e1e9110d8b3fc668aedeeba2da0f2679592b4a
mgmman <115487609+mgmman@users.noreply.github.com>
2024-06-08 19:05:31 +0500

    added more adaptivity

--- backend/DataLayer/Models/Article.cs
+++ backend/DataLayer/Models/Article.cs
@@ -1,6 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace DataLayer.Models;
 
 public class Article
 {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int ArticleId { get; set; }
+
+    [Required]
+    [StringLength(40)]
+    public string Tittle { get; set; }
     
+    public Charity Author { get; set; }
+    
+    public string Text { get; set; }
+    
+    public ArticlePhoto? Photo { get; set; }
 }
